87952eef0949bbfa3919fbe9dd4f9c09c0f69cd3
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-04-17 18:02:18 +0000

    starting React with full potential

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>CHai Aur React | Vansh Angra</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Chai aur React with Vite | Vansh</h1>
+  )
+}
+
+export default App
