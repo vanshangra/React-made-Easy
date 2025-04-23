615957f6091dd09be2b1f6f386fcf38a8d84df82
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-04-23 17:55:34 +0000

    tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -11,8 +11,7 @@ function App() {
     age: 19
   }
 
-  let newArr = [1,3,4,5]
-
+  let newArr = [1,3,4,5
   return (
     <>
       <h1 className = 'bg-green-400 text-black p-4 rounded-xl mb-4'> Tailwind Test</h1>
