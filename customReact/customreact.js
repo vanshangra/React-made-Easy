edbde05f52d13ec6377e03c76f3b6daf665e7f0f
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-06-25 17:26:00 +0000

    commit

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -11,7 +11,8 @@ function customRender(reactElement, mainContainer){
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
     for (const prop in reactElement.props) {
-        if(prop === 'children') continue;
+        if(prop === 'children') 
+            continue;
         domElement.setAttribute(prop,reactElement.props[prop])
     }
     mainContainer.appendChild(domElement)
